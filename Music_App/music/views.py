a699999a8923837b441ad4c4f5e18fb5f7941e9c
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-10 10:09:22 +0200

    Added views , to make URL path

--- Music_App/music/views.py
+++ Music_App/music/views.py
@@ -1,2 +1,29 @@
-
 from django.shortcuts import render
+
+
+def index(request):
+    pass
+
+
+def add_album(request):
+    pass
+
+
+def edit_album(request):
+    pass
+
+
+def delete_album(request):
+    pass
+
+
+def details_album(request):
+    pass
+
+
+def details_profile(request):
+    pass
+
+
+def delete_profile(request):
+    pass
