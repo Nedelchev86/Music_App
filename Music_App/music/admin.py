80a74d99b28bf5a7981be9a1db0002fd0970651b
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-10 08:49:20 +0200

    Added Profile models to create database and validators

--- Music_App/music/admin.py
+++ Music_App/music/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from Music_App.music.models import Profile
+
+
 # Register your models here.
+
+
+@admin.register(Profile)
+class AdminProfile(admin.ModelAdmin):
+    pass
--- Music_App/music/models.py
+++ Music_App/music/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+from Music_App.music.validators.validators import string_validator
+
+
+class Profile(models.Model):
+    username = models.CharField(
+        max_length=15,
+        validators=[MinLengthValidator(2), string_validator],
+        null=False,
+        blank=False,
+    )
+
+    email = models.EmailField(
+        null=False,
+        blank=False
+    )
+
+    age = models.IntegerField(
+        null=True,
+        blank=True,
+        validators=(MinValueValidator(0),)
+    )
+
+    def __str__(self):
+        return self.username
